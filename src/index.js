5e6546fe516fdfdfe4fd04569da931d4244a211d
HyunJin Won <ohj8447@gmail.com>
2021-08-04 20:56:04 +0900

    make custom Component and make module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
+function Avatar(props) {
+    return <image className="Avatar"
+                  src={props.user.avatarUrl}
+                  alt={props.user.name}
+    />;
+}
+
+function UserInfo(props) {
+    return <div className={UserInfo}>
+        <Avatar user={props.user}/>
+        <div className="UserInfo-name">
+            {props.user.name}
+        </div>
+    </div>;
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author}/>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+function formatDate(date) {
+    return date.toLocaleTimeString();
+}
+
+ReactDOM.render(Comment({
+    author: {
+        name: 'hj',
+        avatarUrl: 'none',
+    },
+    text: 'what it is',
+    date: new Date(),
+}), document.getElementById('root'));
+/* Welcome example
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello {this.props.name}</h1>;
+    }
+}
+
+const element = <Welcome name='HJ'/>;
+ReactDOM.render(element, document.getElementById('root'));
+*/
+/* //tick time
 function tick() {
     const element = (
         <div>
@@ -11,8 +64,8 @@ function tick() {
         </div>);
     ReactDOM.render(element, document.getElementById('root'));
 }
-
 setInterval(tick, 1000);
+*/
 /*
 ReactDOM.render(
   <React.StrictMode>
